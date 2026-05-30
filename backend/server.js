05ab1095618ff6a5fb21cbdc031a52d4bed0c130
Souvik Sinhababu <souviksinhababu1@gmail.com>
2026-05-30 16:16:24 +0530

    Push

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,17 @@ const connectDB = require("./db");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
-const admin = require("firebase-admin");
+import admin from "firebase-admin";
+
+// Firebase initialization here
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
+});
 
 dotenv.config();
 connectDB();
@@ -156,7 +166,6 @@ const startServer = (port) => {
   });
 };
 
-import admin from "firebase-admin";
 
 admin.initializeApp({
   credential: admin.credential.cert({
